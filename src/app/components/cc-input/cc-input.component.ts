74b420284755626886f102ec479f996abb0840ca
Ken Larson <servantfist@users.noreply.github.com>
2023-10-04 15:11:09 -0600

    Fixed error of masking cc number before getting cc type.

--- src/app/components/cc-input/cc-input.component.ts
+++ src/app/components/cc-input/cc-input.component.ts
@@ -6,8 +6,7 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./cc-input.component.scss'],
 })
 export class CcInputComponent {
-  /* Oi Vey.  This needs to be done as a pipe so that we are not ruining the current value of the credit card number and are instead transforming it for formatting and masking.
-   * BECAUSE:  If you want to toggle visibility or the ability to clear the value the input can't be 'hard-coded' via property binding.  It needs to be a pipe. */
+  /* Oi Vey.  This needs to be done as a pipe so that we are not ruining the current value of the credit card number and are instead transforming it for formatting and masking. BECAUSE:  If you want to toggle visibility or the ability to clear the value the input can't be 'hard-coded' via property binding.  It needs to be a pipe. */
 
   @Input() public ccNumber = '';
   public ccType = '0';
@@ -18,10 +17,9 @@ export class CcInputComponent {
   }
 
   @Input() public set isReadOnly(value: boolean) {
-    this.readOnly = value;
     this.ccType = this.ccNumber[0];
     this.ccNumber = value ? this.maskCCNumber(this.ccNumber) : this.ccNumber;
-    this.isReadOnly = value;
+    this.readOnly = value;
   }
 
   public getCCNumber(): string {
--- src/app/components/loader/loader.component.ts
+++ src/app/components/loader/loader.component.ts
@@ -1,5 +1,4 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { tap, mergeMap, from, timer, Subject, BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-loader',
