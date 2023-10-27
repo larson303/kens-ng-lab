4bfadff5aa3661f2b4fe3861c5aa7daa7ad2acd0
Ken Larson <applytruth@yahoo.com>
2023-10-27 15:27:54 -0600

    Rename accidentally created second directive with the same name.

--- src/app/directives/debounce-click/debounceclick.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DebounceclickDirective } from './debounceclick.directive';
-
-describe('DebounceclickDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DebounceclickDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/directives/directives.module.ts
+++ src/app/directives/directives.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DirectiveDocsComponent } from './directive-docs.component';
 import { RouterModule } from '@angular/router';
-import { DebounceClickDirective } from './debounce-click/debounce-click.directive';
 import { CardModule } from '../common/card/card.module';
+
+import { DirectiveDocsComponent } from './directive-docs.component';
+import { DebounceClickDirective } from './debounce-click/debounce-click.directive';
 import { DebounceClick2Directive } from './debounce-click2.directive';
 
 const routes = [
